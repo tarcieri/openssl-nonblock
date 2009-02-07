8e51c979ce979eb55764707b7ac7a596a46216af
Tony Arcieri <tony@medioh.com>
2009-02-06 22:28:12 -0700

    Check for rb_str_set_len (needed for Ruby 1.9 support

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -8,4 +8,10 @@ if have_header('openssl/ssl.h')
   $LIBS << '-lssl -lcrypto'
 end
 
+if have_func('rb_str_set_len')
+  $defs << '-DHAVE_RB_STR_SET_LEN'
+end
+
+$defs << "-DRUBY_VERSION_CODE=#{RUBY_VERSION.gsub(/\D/, '')}"
+
 create_makefile("openssl_nonblock")
